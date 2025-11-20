94e7517c5d85237f774b6d934616dee22cc6d957
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-11-20 23:11:08 +0900

    remake main

--- trackMaker/main.py
+++ trackMaker/main.py
@@ -2,122 +2,67 @@ import numpy as np
 import pybullet as p
 import pybullet_data as pd
 
-import road_mesh_generator as rmg
 
-#*========== basic settings ==============
-#* camera params
-target_pos = [0, 0, 0] #view origin
-camera_pos = [0, 0, 5] #(x, y, z)
+#*=========== basic settings ==============
+def init_engine():
+    engine_id = p.connect(p.GUI)
+    p.setAdditionalSearchPath(pd.getDataPath())
+    p.setGravity(0, 0, -9.8)
+    p.resetSimulation()
 
-#* road params
-road_thicknes = 0.05
-road_with = 2.0
-road_length = 2.0
+    return engine_id
 
-#*========== init engine =================
-engine_id = p.connect(p.GUI)
-p.setAdditionalSearchPath(pd.getDataPath())
-p.setGravity(0, 0, -9.8)
-
-view_matrix = p.computeViewMatrix(
-    cameraEyePosition=camera_pos,
-    cameraTargetPosition=target_pos,
-    cameraUpVector=[1, 0, 0]   # 上方向ベクトル（真下を見るときは水平成分が必要）
-)
-
-proj_matrix = p.computeProjectionMatrixFOV(
-    fov=60, aspect=1.0, nearVal=0.1, farVal=100.0
-)
-
-p.resetSimulation()
-
-#* make Ground 
-ground_coll = p.createCollisionShape(p.GEOM_BOX, halfExtents=[50, 50, 0.1])
-ground_vis = p.createVisualShape(
-    p.GEOM_BOX,
-    halfExtents=[50, 50, 0.1],
-    rgbaColor=[0.6, 0.8, 0.6, 1]
-) 
-
-p.createMultiBody(
-    baseMass=0,
-    baseCollisionShapeIndex=ground_coll,
-    baseVisualShapeIndex=ground_vis,
-    basePosition=[0, 0, -0.1]
-)
-
-#*======= road generate function =================
-def create_straight(x, y, yaw, length=road_length):
-    """generate straight road"""
-    return p.createMultiBody(
+#*=========== road generate =============
+def gen_straight(x, y, length=5, width=1, step=0.1):
+    col_id = p.createCollisionShape(
+        shapeType=p.GEOM_BOX, 
+        halfExtents=[length / 2, width / 2, 0.06]
+    )
+    vis_id = p.createVisualShape(
+        shapeType=p.GEOM_BOX,
+        halfExtents=[length / 2, width / 2, 0.06],
+        rgbaColor=[0.3, 0.3, 0.3, 1],
+        specularColor=[0, 0, 0]
+    )
+    road = p.createMultiBody(
         baseMass=0,
-        baseCollisionShapeIndex=road_coll,
-        baseVisualShapeIndex=road_vis,
-        basePosition=[x, y, road_thicknes / 2],
-        baseOrientation=p.getQuaternionFromEuler([0, 0, yaw])
+        baseCollisionShapeIndex=col_id,
+        baseVisualShapeIndex=vis_id,
+        basePosition=[x, y, 0]
     )
 
-def create_curve(radius, start_angle, end_angle, arc_step=0.05):
-    arc_length = radius * abs(end_angle - start_angle)
-    num_points = max(2, int(arc_length / arc_step))
+# def gen_curve():
 
-    angle_list = np.linspace(start_angle, end_angle, num_points)
-
-    points = []
-    #* calculate coordinate of each point
-    for angle in range(angle_list):
-        x = radius * np.cos(angle)
-        y = radius * np.sin(angle)
-
-        points.append([x, y, 0])
-
-    return np.array(points)
-
-#* make Road
-
-road_coll = p.createCollisionShape(
-    p.GEOM_BOX,
-    halfExtents=[road_length/2, road_with / 2, road_thicknes / 2]
-)
-
-road_vis = p.createVisualShape(
-    p.GEOM_BOX,
-    halfExtents=[road_length / 2, road_with / 2, road_thicknes / 2],
-    rgbaColor=[0.3, 0.3, 0.3, 1]
-)
-
-# vertices, faces = rmg.gen_road_mesh()
+# def gen_curve_points(r, start_theta, end_theta):
+    
 
 
-#* Automatic Course Generater
-course_ids = []
-for i in range(5):
-    course_ids.append(create_straight(x= i * road_length * 0.9, y=0, yaw=0))
-
-# course_ids += create_curve(radius=5, angle=np.pi / 2, segments=20, start_angle=0)
-
-
-#* Collision Detection to road (inside or outside)
-"""
-! I wonder make a program which do determine contact for each wheels.
-"""
-def is_on_road(car_id, road_ids):
-    """Determine whether car is inside or outside on road."""
-    for rid in road_ids:
-        contacts = p.getContactPoints(car_id, rid)
-        if len(contacts) > 0:
-            return True
-        
-    return False
-
-
-
+#*=========== main func ==============
 def main():
-    print("\n<Welcome>\n")
+    print("\n~~ WELCOME ~~\n")
+
+    init_engine()
+
+    #* generate field
+    field_col_id = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[25, 25, 0.05])
+    field_vis_id = p.createVisualShape(
+        shapeType=p.GEOM_BOX, 
+        halfExtents=[25, 25, 0.05], 
+        rgbaColor=[0.6, 0.8, 0.6, 1], 
+        specularColor=[0, 0, 0]
+    )
+    field = p.createMultiBody(
+        baseMass=0, 
+        baseCollisionShapeIndex=field_col_id,
+        baseVisualShapeIndex=field_vis_id,
+        basePosition=[0, 0, 0] 
+    )
+
+    gen_straight()
 
     while True:
-        p.stepSimulation()
-        
-        
+        p.stepSimulation(0, 0, 5)
+
+
 if __name__ == "__main__":
     main()
