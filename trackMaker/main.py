afc9c44407991e76dbb15ce1d1d66aa16b4e13ae
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-11-20 23:57:45 +0900

    edit main

--- trackMaker/main.py
+++ trackMaker/main.py
@@ -31,9 +31,50 @@ def gen_straight(x, y, length=5, width=1, step=0.1):
         basePosition=[x, y, 0]
     )
 
-# def gen_curve():
+def gen_arc_points(r, start_theta, end_theta, step=0.1):
+    arc_length = r * abs(end_theta - start_theta)
+    points_num = max(2, int(arc_length / step))
+    theta_list = np.linspace(start_theta, end_theta, points_num)
 
-# def gen_curve_points(r, start_theta, end_theta):
+    points = []
+    for theta in theta_list:
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
+
+        points.append([x, y, 0])
+    
+    return np.array(points)
+
+
+def gen_curve(points, width=1):
+    """
+    points => [x, y, 0]
+    """
+
+    L_offset = []
+    R_offset = []
+
+    N = len(points)
+
+    #* culc vector
+    for i in range(points):
+        #* Tangent
+        if i == 0:
+            tan_vec = points[1] - points[0]
+        elif i == (N-1):
+            tan_vec = points[i] - points[i-1]
+        else :
+            tan_vec = points[i+1] - points[i-1]
+        
+        unit_tan_vec = tan_vec / np.linalg.norm(tan_vec)
+
+        #* Normal
+        normal = np.array([-unit_tan_vec[1], unit_tan_vec[0], 0])
+
+        L_offset.append(points[i] + normal * (width / 2))
+        R_offset.append(points[i] - normal * (width / 2))
+
+    #* Generate triangle mesh
     
 
 
