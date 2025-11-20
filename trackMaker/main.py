c6b100621f87079d1b4cbb1b7d227504f1b41cea
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-11-20 18:54:33 +0900

    edit main.py

--- trackMaker/main.py
+++ trackMaker/main.py
@@ -2,12 +2,33 @@ import numpy as np
 import pybullet as p
 import pybullet_data as pd
 
+import road_mesh_generator as rmg
 
+#*========== basic settings ==============
+#* camera params
+target_pos = [0, 0, 0] #view origin
+camera_pos = [0, 0, 5] #(x, y, z)
 
-#* init engine
+#* road params
+road_thicknes = 0.05
+road_with = 2.0
+road_length = 2.0
+
+#*========== init engine =================
 engine_id = p.connect(p.GUI)
 p.setAdditionalSearchPath(pd.getDataPath())
 p.setGravity(0, 0, -9.8)
+
+view_matrix = p.computeViewMatrix(
+    cameraEyePosition=camera_pos,
+    cameraTargetPosition=target_pos,
+    cameraUpVector=[1, 0, 0]   # 上方向ベクトル（真下を見るときは水平成分が必要）
+)
+
+proj_matrix = p.computeProjectionMatrixFOV(
+    fov=60, aspect=1.0, nearVal=0.1, farVal=100.0
+)
+
 p.resetSimulation()
 
 #* make Ground 
@@ -25,23 +46,7 @@ p.createMultiBody(
     basePosition=[0, 0, -0.1]
 )
 
-#* make Road
-road_thicknes = 0.05
-road_with = 2.0
-road_length = 2.0
-
-road_coll = p.createCollisionShape(
-    p.GEOM_BOX,
-    halfExtents=[road_length/2, road_with / 2, road_thicknes / 2]
-)
-
-road_vis = p.createVisualShape(
-    p.GEOM_BOX,
-    halfExtents=[road_length / 2, road_with / 2, road_thicknes / 2],
-    rgbaColor=[0.3, 0.3, 0.3, 1]
-)
-
-#* road generate function
+#*======= road generate function =================
 def create_straight(x, y, yaw, length=road_length):
     """generate straight road"""
     return p.createMultiBody(
@@ -68,6 +73,20 @@ def create_curve(radius, start_angle, end_angle, arc_step=0.05):
 
     return np.array(points)
 
+#* make Road
+
+road_coll = p.createCollisionShape(
+    p.GEOM_BOX,
+    halfExtents=[road_length/2, road_with / 2, road_thicknes / 2]
+)
+
+road_vis = p.createVisualShape(
+    p.GEOM_BOX,
+    halfExtents=[road_length / 2, road_with / 2, road_thicknes / 2],
+    rgbaColor=[0.3, 0.3, 0.3, 1]
+)
+
+# vertices, faces = rmg.gen_road_mesh()
 
 
 #* Automatic Course Generater
@@ -75,7 +94,7 @@ course_ids = []
 for i in range(5):
     course_ids.append(create_straight(x= i * road_length * 0.9, y=0, yaw=0))
 
-course_ids += create_curve(radius=5, angle=np.pi / 2, segments=20, start_angle=0)
+# course_ids += create_curve(radius=5, angle=np.pi / 2, segments=20, start_angle=0)
 
 
 #* Collision Detection to road (inside or outside)
@@ -98,7 +117,7 @@ def main():
 
     while True:
         p.stepSimulation()
-
+        
         
 if __name__ == "__main__":
     main()
