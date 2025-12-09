393a2d6f1226881c83141f94ce3b8ffc62855254
Hirakuhito <takuto.yoshino1210@icloud.com>
2025-12-09 13:08:32 +0900

    -

--- trackMaker/main.py
+++ trackMaker/main.py
@@ -2,10 +2,13 @@ import numpy as np
 import pybullet as p
 import pybullet_data as pd
 
+import vertex_data_generator as vdg
+
 
 def main():
     print("\n~ Welcome ~\n")
 
+    vdg.generate_straight_track()
 
 if __name__ == "__main__":
     main()
